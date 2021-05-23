175f289ca2f0854f3d31894cf45b4b06a6da95a8
Jasmine Kay <jasminekay505@gmail.com>
2021-05-23 11:48:17 +0800

    update icon and add jumbotron

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./style.css";
+import headshot from "../../assets/headshot.jpg"
 
-
+function Header() {
+    return (
+        <div className="jumbtron text-center">
+            <h1 className="display-4">
+                JASMINE SANCHEZ
+            </h1>
+            <img className="headshot" src={headshot} />
+            <p className="lead">Mechanical Engineer | Software Engineer</p>
+        </div>
+    )
+}
 
 export default Header;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Nav from "../components/NavBar";
+import Header from "../components/Header";
 
-function About () { 
-    return ( 
-        <Nav/>
+function About() {
+    return (
+        <div>
+            <Nav />
+            <Header />
+        </div>
+
     )
 }
 
