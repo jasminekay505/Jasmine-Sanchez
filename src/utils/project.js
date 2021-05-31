f52ece776341fdfd464ebae46a2d790af6378067
Jasmine Kay <jasminekay505@gmail.com>
2021-05-31 20:50:39 +0800

    setting up project infor

---
+++ src/utils/project.js
@@ -0,0 +1,10 @@
+//import pictures
+
+function ProjectCard (title, description, img, appLink, gitHubLink ) { 
+    this.title = title;
+    this.description = description;
+    this.img = img;
+    this.appLink = appLink;
+    this.gitHubLink = gitHubLink;
+};
+
