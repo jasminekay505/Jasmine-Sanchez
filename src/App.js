11615269a0edd44b897e4f6dfbe104f3dae2927b
Jasmine Kay <jasminekay505@gmail.com>
2021-06-01 10:14:37 +0800

    removing browser routers and adding homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Projects from "./pages/Project";
 import './index.css';
